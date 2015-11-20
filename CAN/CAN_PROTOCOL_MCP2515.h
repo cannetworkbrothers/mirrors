b24386eeaabe62a03443d064b151f0d0bbf4483b
Dima <dima@ex.com>
2015-11-20 16:18:58 +0200

    define cmd command for mcp2515

--- CAN/CAN_PROTOCOL_MCP2515.h
+++ CAN/CAN_PROTOCOL_MCP2515.h
@@ -16,6 +16,17 @@
 #define MCP2515_SS_0 PORTB &= ~(1 << PINB2 )
 #define MCP2515_SS_1 PORTB |= (1 << PINB2)
 
+// command definitions
+#define MCP2515_CMD_RESET 0xC0
+#define MCP2515_CMD_READ 0x03
+#define MCP2515_CMD_WRITE 0x02
+#define MCP2515_CMD_BIT_MODIFY 0x05
+#define MCP2515_CMD_READ_STATUS 0xA0
+#define MCP2515_CMD_LOAD_TX 0x40
+#define MCP2515_CMD_RTS 0x80
+#define MCP2515_CMD_RX_STATUS 0xB0
+#define MCP2515_CMD_READ_RX 0x90
+
 
 
 class MCP2515
