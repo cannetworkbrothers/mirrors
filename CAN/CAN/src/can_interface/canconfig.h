7149ad4918c608b853a7d7e4632faee6f2aebda2
Igor <ivkovalenko@gmail.com>
2016-04-21 22:26:46 +0300

    iss5_added_canconfig.h

---
+++ CAN/CAN/src/can_interface/canconfig.h
@@ -0,0 +1,25 @@
+/*
+ * canconfig.h
+ *
+ * Created: 4/16/2016 11:26:45 PM
+ *  Author: Igor
+ */ 
+
+
+#ifndef CANCONFIG_H_
+#define CANCONFIG_H_
+
+#define SPI_CS_PORT_NAME		B
+#define SPI_CS_PORT_BIT			2
+
+#define SPI_MOSI_PORT_NAME		B
+#define SPI_MOSI_PORT_BIT		3
+
+#define SPI_MISO_PORT_NAME		B
+#define SPI_MISO_PORT_BIT		4
+
+#define SPI_SCK_PORT_NAME		B
+#define SPI_SCK_PORT_BIT		5
+
+
+#endif /* CANCONFIG_H_ */
